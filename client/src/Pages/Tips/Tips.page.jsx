974c54e22d6ad9815deb96e47b7263da977699fe
xennia316 <xennia316@gmail.com>
2022-11-10 02:37:30 +0100

    minor changes

--- client/src/Pages/Tips/Tips.page.jsx
+++ client/src/Pages/Tips/Tips.page.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { NavBar } from "../../Components";
 import { TipSection } from "../../Sections";
 
 const Tips = () => {
   return (
     <section>
+      <NavBar />
       <TipSection />
     </section>
   );
