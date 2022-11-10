ec76460b7057fb528adf56ca4571229a3edee734
xennia316 <xennia316@gmail.com>
2022-11-10 06:51:48 +0100

    attempted correction

--- client/src/Sections/Hero/Hero.section.jsx
+++ client/src/Sections/Hero/Hero.section.jsx
@@ -1,23 +1,29 @@
 import React from "react";
 import { Button } from "../../Components";
+import image from "../../Resources/hero_image.png";
 import styles from "./Hero.module.css";
 
 const Hero = () => {
   return (
     <section className={`${styles.HeroLander}`}>
-      <section className={`${styles.bigText}`}>
-        LET'S GET <br />
-        TO THE DIRTY
+      <section className={styles.left}>
+        <section className={`${styles.bigText}`}>
+          LET'S GET <br />
+          TO THE DIRTY
+          <br />
+          BUSINESS
+        </section>
         <br />
-        BUSINESS
+        <section className={`${styles.smallText}`}>
+          We provide you with the most updated routes <br />
+          taken by the waste collecting company every week
+        </section>
+        <br />
+        <Button props={"See Routes"} />
       </section>
-      <br />
-      <section className={`${styles.smallText}`}>
-        We provide you with the most updated routes <br />
-        taken by the waste collecting company every week
+      <section className={styles.image}>
+        <img src={image} alt="hero image" />
       </section>
-      <br />
-      <Button props={"See Routes"} />
     </section>
   );
 };
